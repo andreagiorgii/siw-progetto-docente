af1726821b76c4c0fd3fd4ace655e24068eaaaf3
andreagiorgii <andrea.giorgi@ticketoo.it>
2022-06-11 18:00:07 +0200

    fix messages

--- Catering/src/main/java/it/uniroma3/siw/catering/controller/validator/UserValidator.java
+++ Catering/src/main/java/it/uniroma3/siw/catering/controller/validator/UserValidator.java
@@ -12,29 +12,30 @@ import it.uniroma3.siw.catering.model.User;
 @Component
 public class UserValidator implements Validator {
 
-	final Integer MAX_NAME_LENGTH = 100;
-	final Integer MIN_NAME_LENGTH = 2;
+    final Integer MAX_NAME_LENGTH = 100;
+    final Integer MIN_NAME_LENGTH = 2;
 
-	@Override
-	public void validate(Object o, Errors errors) {
-		User user = (User) o;
-		String nome = user.getNome().trim();
-		String cognome = user.getCognome().trim();
+    @Override
+    public void validate(Object o, Errors errors) {
+        User user = (User) o;
+        String nome = user.getNome().trim();
+        String cognome = user.getCognome().trim();
 
-		if (nome.isEmpty())
-			errors.reject("nome.required");
-		else if (nome.length() < MIN_NAME_LENGTH || nome.length() > MAX_NAME_LENGTH)
-			errors.reject("nome.size");
+        if (nome.isEmpty())
+            errors.rejectValue("nome", "required");
+        else if (nome.length() < MIN_NAME_LENGTH || nome.length() > MAX_NAME_LENGTH)
+            errors.rejectValue("nome", "size");
 
-		if (cognome.isEmpty())
-			errors.reject("cognome.required");
-		else if (cognome.length() < MIN_NAME_LENGTH || cognome.length() > MAX_NAME_LENGTH)
-			errors.reject("cognome.size");
-	}
+        if (cognome.isEmpty())
+            errors.rejectValue("cognome", "required");
+        else if (cognome.length() < MIN_NAME_LENGTH || cognome.length() > MAX_NAME_LENGTH)
+            errors.rejectValue("cognome", "size");
+    }
 
-	@Override
-	public boolean supports(Class<?> clazz) {
-		return User.class.equals(clazz);
-	}
+    @Override
+    public boolean supports(Class<?> clazz) {
+        return User.class.equals(clazz);
+    }
 
 }
+

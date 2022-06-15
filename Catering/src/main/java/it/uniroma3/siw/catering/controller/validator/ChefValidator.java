685218ca5b1a448ca33050ddb33eab55332232e9
andreagiorgii <andrea.giorgi@ticketoo.it>
2022-06-15 19:20:20 +0200

    add validator chef

--- Catering/src/main/java/it/uniroma3/siw/catering/controller/validator/ChefValidator.java
+++ Catering/src/main/java/it/uniroma3/siw/catering/controller/validator/ChefValidator.java
@@ -4,26 +4,30 @@ import org.springframework.stereotype.Component;
 import org.springframework.validation.Errors;
 import org.springframework.validation.Validator;
 
-import it.uniroma3.siw.catering.model.Buffet;
+import it.uniroma3.siw.catering.model.Chef;
 import it.uniroma3.siw.catering.model.User;
 
 /**
  * Validator Buffet
  */
 @Component
-public class BuffetValidator implements Validator {
+public class ChefValidator implements Validator {
 
 	@Override
 	public void validate(Object o, Errors errors) {
-		Buffet buffet = (Buffet) o;
-		String nome = buffet.getNome().trim();
-		String descrizione = buffet.getDescrizione().trim();
+		Chef chef = (Chef) o;
+		String nome = chef.getNome().trim();
+		String cognome = chef.getCognome().trim();
+		String nazionalita = chef.getNazionalita().trim();
 
 		if (nome.matches(".*\\d.*"))
 			errors.rejectValue("nome", "isNumber");
 
-		if (descrizione.matches(".*\\d.*"))
-			errors.rejectValue("descrizione", "isNumber");
+		if (cognome.matches(".*\\d.*"))
+			errors.rejectValue("cognome", "isNumber");
+		
+		if (nazionalita.matches(".*\\d.*"))
+			errors.rejectValue("nazionalita", "isNumber");
 	}
 
 	@Override
